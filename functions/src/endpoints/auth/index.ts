024ee56f4dcc9fc4e6af745d912666e0cf50f675
Xingbin <xingbingao@gmail.com>
2025-04-09 11:16:29 +1200

    Improve deployment

--- functions/src/endpoints/auth/index.ts
+++ functions/src/endpoints/auth/index.ts
@@ -1,8 +1,5 @@
 import { Router as createRouter } from 'express';
-import register from './register';
 
 const router = createRouter();
-
-router.post('/register', register);
 
 export default router;
